c01005765b31c1fb266a24263be45d0a61a5c404
Muhammad Iqbal <iqbalgooner33@gmail.com>
2023-01-16 14:49:28 +0700

    division seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             PermissionTableSeeder::class,
             RoleTableSeeder::class,
             UserTableSeeder::class,
+            DivisionTableSeeder::class,
         ]);
     }
 }
--- database/seeders/DivisionTableSeeder.php
+++ database/seeders/DivisionTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DivisionTableSeeder extends Seeder
 {
@@ -14,6 +15,22 @@ class DivisionTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('divisions')->insert([
+            [
+                'division_name' => 'IT',
+            ],
+            [
+                'division_name' => 'GA',
+            ],
+            [
+                'division_name' => 'HRD',
+            ],
+            [
+                'division_name' => 'Finace',
+            ],
+            [
+                'division_name' => 'Marketing'
+            ]
+        ]);
     }
 }
